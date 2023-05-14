5d73caeeca885faa6cd1106140d296e430f60349
sargaraditya <91034566+sargaraditya@users.noreply.github.com>
2023-05-14 18:24:14 -0400

    Add files via upload

---
+++ src/main/java/com/aditya/employeemanager/service/EmployeeService.java
@@ -0,0 +1,40 @@
+package com.aditya.employeemanager.service;
+
+import com.aditya.employeemanager.exception.UserNotFoundException;
+import com.aditya.employeemanager.model.Employee;
+import com.aditya.employeemanager.repo.EmployeeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepo employeeRepo;
+    @Autowired
+    public EmployeeService(EmployeeRepo employeeRepo) {
+        this.employeeRepo = employeeRepo;
+    }
+
+    public Employee addEmployee(Employee employee) {
+        employee.setEmployeeCode(UUID.randomUUID().toString());
+        return employeeRepo.save(employee);
+    }
+
+    public List<Employee> findAllEmployees(){
+        return employeeRepo.findAll();
+    }
+
+    public Employee updateEmployee(Employee employee) {
+        return employeeRepo.save(employee);
+    }
+
+    public Employee findEmployeeById(Long id) {
+        return employeeRepo.findEmployeeById(id).orElseThrow(() -> new UserNotFoundException("User by id" + id + "was not Found"));
+    }
+
+    public void deleteEmployee(Long id) {
+        employeeRepo.deleteEmployeeById(id);
+    }
+}
